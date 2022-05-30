78cde1f16b4ae0321d6f2fda5a7220bb99c208a6
Pranudech <pranudech@gmail.com>
2022-05-30 16:14:06 +0700

    update chat

--- components/Chat.js
+++ components/Chat.js
@@ -3,8 +3,8 @@ import { useState, useContext, useEffect } from 'react'
 // import { GunContext } from '../context/gunContext'
 // import { faker } from '@faker-js/faker'
 import ChevronUp from '../assets/svg/chevronUp'
-// import ChatCard from './ChatCard'
-// import Button from './Button'
+import ChatCard from './ChatCard'
+import Button from './Button'
 import shiba from '../assets/shiba.png'
 import Image from 'next/image'
 
@@ -28,8 +28,98 @@ const Chat = () => {
     const [bullishValue, setBullishValue] = useState(true)
 
 
+    const sendMessage = () => {
+        if (message.trim() === '') return
+    }
+
     return (
-        <div>Chat</div>
+        <>
+            <div className={styles.chat}>
+                <div className={styles.flexBetween}>
+                    <p className={styles.boldText}>Live Shiba Inu Chat</p>
+                    <p className='text-[#6188FF]'>See more</p>
+                </div>
+
+                <br />
+
+                <div className={styles.chatContainer}>
+                    <div className={styles.flexBetween}>
+                        <div className={styles.flexCenter}>
+                            <div>
+                                <Image alt='' src={shiba} width={70} height={70} />
+                            </div>
+                            <div className='text-left mr-10'>
+                                <b>Drak</b>
+                                <p className='text-gray-400'>@drakosi</p>
+                            </div>
+                        </div>
+
+                        <div className={styles.flexCenter}>
+                            <div
+                                className={
+                                    !bullishValue
+                                        ? styles.bullishLabel
+                                        : styles.activeBullishLabel
+                                }
+                                onClick={() => setBullishValue(true)}
+                            >
+                                <ChevronUp fill='#17C784' />
+                                <small className='ml-1'>Bullish</small>
+                            </div>
+                            &nbsp; &nbsp;
+                            <div
+                                className={
+                                    bullishValue ? styles.bearishLabel : styles.activeBearishLabel
+                                }
+                                onClick={() => setBullishValue(false)}
+                            >
+                                <ChevronDown fill='#a52b2b' />
+                                <small className='ml-1'>Bearish</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='flex'>
+                <div className='flex items-center text text-green-600'>
+                    <ChevronUp fill='#22bc64' />
+                    <small className='ml-1'>Bullish</small>
+                </div>
+                &nbsp; &nbsp;
+                <div className='flex items-center text-red-500'>
+                    <ChevronDown fill='#a52b2b' />
+                    <small className='ml-1'>Bearish</small>
+                </div>
+            </div>
+
+            <input
+                className={styles.input}
+                placeholder="What's happening on BTC?"
+                value={message}
+                onChange={e => setMessage(e.target.value)}
+            />
+
+            <div className={styles.postButtonContainer}>
+                <Button label='Post' onPress={sendMessage} />
+            </div>
+            {/* {formattedMessagesArray()
+                .slice(0)
+                .reverse()
+                .map((message, index) => (
+                    <ChatCard
+                        key={index}
+                        sender={message.sender}
+                        senderUsername={message.username}
+                        senderAvatar='https:/encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3OCSMFIW5fZ3vSN6yGpD-w-6SsL2_ZPA_sw&usqp=CAU'
+                        bullish={message.isBullish}
+                        timestamp={message.createdAt}
+                        content={message.content}
+                        likes='2.7K'
+                        comments='19K'
+                    />
+                ))} */}
+        </>
     )
 }
 
